2568657612180e3fd0607983961f89427eecf276
NandlalKumar615 <nandlalkumar615@gmail.com>
2023-12-25 19:37:55 +0530

    u2

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
-
-import 'https://unpkg.com/boxicons@2.1.4/dist/boxicons.js';
+import 'boxicons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
